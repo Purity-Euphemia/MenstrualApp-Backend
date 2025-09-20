04b5b2a4a18d1cb224ff0a841e3cf30c5868554f
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-20 20:51:02 +0100

    first commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,3 +3,22 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7h" });
+
+exports.register = async (req, res) => {
+    const { email, password } = req.body;
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(401).json({ message: "User already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ email, password: hashed });
+
+    res.json({ token: generateToken(user._id), user: user });
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+}

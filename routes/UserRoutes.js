35d22dc85afeca5b235d3e643d44801afb28c3ff
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-21 22:43:40 +0100

    first commit

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,3 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { register, login, getSettings, saveSettings} = require("../controllers/userController");
 const protect = require("../middleware/auth");
+
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/settings", protect, getSettings);
+router.post("/settings", protect, getSettings);
+
+module.exports = router;
